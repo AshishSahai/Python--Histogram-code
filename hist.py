73ae03078c1f5f5b7215fb1d40f5536c396ab1bf
Ashish Kumar <ashish0981kumar@gmail.com>
2025-01-28 13:14:31 +0530

    Create hist.py
    creates histogram (showing number of repetitions of an element)

---
+++ hist.py
@@ -0,0 +1,9 @@
+counts = dict()
+names = ['csev', 'zwen', 'ken', 'ten']
+for name in names:
+  if name not in counts:
+     count[name] = 1
+  else:
+     count[name] = count[name] + 1
+print(counts)
+
